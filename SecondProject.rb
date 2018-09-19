e72ae3fb84a0896483ad200ce59dd11697c19260
aljarrell <alan.k.jarrell@gmail.com>
2018-09-19 18:13:58 -0400

    Added Sounds to the functions

--- SecondProject.rb
+++ SecondProject.rb
@@ -1,23 +1,28 @@
 # Learn to CODE
 
 def Wake_Up(alarm)
-  
+  sample alarm
+  sleep(1)
 end
 
 def Breakfast(eat)
-  
+  sample eat
+  sleep(1)
 end
 
 def Boot_PC(power)
-  
+  sample power
+  sleep(1)
 end
 
-def Win(CODE)
-  
+def Win(code)
+  play(code)
 end
 
 2.times do
-  
+  Wake_Up(:bass_hit_c)
+  Breakfast(:guit_harmonics)
+  Boot_PC(:ambi_choir)
+  Win(90)
 end
-  
-  
+
