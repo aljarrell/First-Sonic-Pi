fef9032d97d86dea2ce2fd15dd1041c6d087bbef
aljarrell <alan.k.jarrell@gmail.com>
2018-09-19 18:47:00 -0400

    Added array and conditional to finish program

--- SecondProject.rb
+++ SecondProject.rb
@@ -16,13 +16,31 @@ def Boot_PC(power)
 end
 
 def Win(code)
-  play(code)
+  if code >= 5
+    sample :bass_trance_c
+    sleep(1)
+  else
+    sample :drum_roll, rate: code
+    sleep(1)
+    Win(code + 1)
+  end
+end
+
+def error()
+  1 == [1,2].choose
+end
+
+def learn()
+  sample :ambi_dark_woosh
+  sleep(2)
 end
 
 2.times do
   Wake_Up(:bass_hit_c)
   Breakfast(:guit_harmonics)
   Boot_PC(:ambi_choir)
-  Win(90)
+  learn if error
+  Win(1)
+  
 end
 
